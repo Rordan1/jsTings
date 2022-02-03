bab321c3871b73fc993fbc679490011eda183bee
CyberRor <87041881+Rordan1@users.noreply.github.com>
2022-02-03 11:31:03 -0500

    trying things out

---
+++ app.js
@@ -0,0 +1,24 @@
+let dog = {
+    race: 'aussi',
+    speed: 'fast',
+    engine: {
+        size: 'medium',
+        make: 'alabama',
+        fuel: 'protein',
+        teeth: [{type: 'sharp'}, {color: 'white'}]
+    },
+    drive: function () {
+        return "run";
+    }
+};
+
+let array = [
+    "string",
+    ["embeded", 200],
+]
+
+console.log(array);
+
+
+
+
